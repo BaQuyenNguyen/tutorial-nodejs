eb7ad4e595643a6c95ba780443e7eb0abd2100a2
Nguyen Ba Quyen <baquyennguyen94@gmail.com>
2024-06-11 21:14:06 +0700

    implement express feature

--- 2-express-tutorial/app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-
-var server = http.createServer((req, res) => {
-    res.writeHead(200, 'content-type', 'text/plain')
-    res.write('<h1>Home Page</h1>')
-    res.end()
-})
-
-server.listen(5000)
