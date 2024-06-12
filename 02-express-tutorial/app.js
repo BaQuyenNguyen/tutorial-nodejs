9b47364c7035f1a4c55c379607becc9a3fac0b1c
Nguyen Ba Quyen <baquyennguyen94@gmail.com>
2024-06-12 09:19:45 +0700

    learn middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,51 +1,32 @@
 const express = require("express");
 const app = express();
-const { products } = require("./data");
+const morgan = require('morgan')
+const logger = require('./logger')
+const authorize = require('./authorize')
+// req => middleware => res
 
-app.get("/", (req, res) => {
-  res.send('<h1> Home Page </h1><a href="/api/products">products</a>');
-});
-app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.json(newProducts);
-});
-app.get("/api/products/:productID", (req, res) => {
-  const { productID } = req.params;
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  );
-  if (!singleProduct) {
-    return res.status(404).send("Product Does Not Exist");
-  }
-  res.json(singleProduct);
-});
+// 1. use vs route
+// 2. options - our own / express / third party
 
-app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
-  console.log(req.params);
-  res.send("hello world");
-});
+// app.use([logger, authorize])
+// app.use(express.static('./public'))
+app.use(morgan('tiny'))
+app.get('/', (req, res) => {
+  res.send('Home Page')
+})
 
-app.get("/api/v1/query", (req, res) => {
-    const {search, limit} = req.query
-    let sortedProducts = [...products]
+app.get('/about', (req, res) => {
+  res.send('About Page')
+})
 
-    if(search){
-        sortedProducts = sortedProducts.filter((product) => {
-            return product.name.startsWith(search)
-        })
-    }
-    if(limit){
-        sortedProducts = sortedProducts.slice(0, Number(limit))
-    }
-    if(sortedProducts.length < 1){
-        return res.status(200).send({success: true, data: []})
-    }
-    res.status(200).json(sortedProducts)
-    // res.send('hello world')
-});
+app.get('/api/products', (req, res) => {
+  res.send('Products')
+})
+
+app.get('/api/items', (req, res) => {
+  console.log(req.user);
+  res.send('Items')
+})
 
 app.listen(5000, () => {
   console.log("Server is listening on port 5000....");
