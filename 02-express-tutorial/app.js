411b31df26b60c56d884280cf7f5e8ee813411ff
Nguyen Ba Quyen <baquyennguyen94@gmail.com>
2024-06-12 18:30:58 +0700

    add methods

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,31 +1,31 @@
 const express = require("express");
 const app = express();
-const morgan = require('morgan')
-const logger = require('./logger')
-const authorize = require('./authorize')
-// req => middleware => res
+let {people} = require('./data')
 
-// 1. use vs route
-// 2. options - our own / express / third party
-
-// app.use([logger, authorize])
-// app.use(express.static('./public'))
-app.use(morgan('tiny'))
-app.get('/', (req, res) => {
-  res.send('Home Page')
+// static assets
+app.use(express.static('./methods-public'))
+// parse form data
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.get('/api/people', (req, res) => {
+  res.status(200).json({success: true, data: people})
 })
 
-app.get('/about', (req, res) => {
-  res.send('About Page')
+app.post('/api/people', (req, res) => {
+  const {name} = req.body
+  if(!name){
+    return res.status(300).json({success: false, msg: 'Please provide name value'})
+  }
+  res.status(201).send({success: true, person: name})
 })
 
-app.get('/api/products', (req, res) => {
-  res.send('Products')
-})
-
-app.get('/api/items', (req, res) => {
-  console.log(req.user);
-  res.send('Items')
+app.post('/login', (req, res) => {
+  console.log(req.body);
+  const {name} = req.body
+  if(name){
+    return res.status(200).send(`Welcome ${name}`)
+  }
+  return res.status(401).send('Please Provide Credentials')
 })
 
 app.listen(5000, () => {
